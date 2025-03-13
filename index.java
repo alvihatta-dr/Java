17019227b62d0c08fb594725af17499cbf8a04a0
Alvi Hatta <142329053+alvihatta-dr@users.noreply.github.com>
2025-03-13 21:55:26 +0700

    App.java

--- index.java
+++ index.java
@@ -1,1 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        PaymentOVO ovoPayment = new PaymentOVO("123456", 50000, "081234567890");
+        PaymentGoPay gopayPayment = new PaymentGoPay("654321", 75000, "089876543210");
 
+        ovoPayment.pay();  // Memanggil metode pay() dari PaymentOVO
+        gopayPayment.pay();  // Memanggil metode pay() dari PaymentGoPay
+    }
+}
